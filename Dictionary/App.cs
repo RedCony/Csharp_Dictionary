2283e68084e80462e5a8defab49e2c2cb3c71097
RedCony <68505636+RedCony@users.noreply.github.com>
2022-01-14 00:37:02 +0900

    최초생성파일

---
+++ Dictionary/App.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Dictionary
+{
+    class App
+    {
+        public App()
+        {
+            string Quest = File.ReadAllText("./QuestList.json");
+          // Console.WriteLine(Quest);
+
+            ItemData[] Questdatas = JsonConvert.DeserializeObject<ItemData[]>(Quest);
+
+            Dictionary<int,ItemData> dicItemDatas = new Dictionary<int,ItemData>();
+
+            foreach (ItemData itemData in Questdatas)
+            {
+                //Console.WriteLine("{0},{1},{2},{4}", itemData.ID, itemData.name,itemData.level,itemData.goal);
+                dicItemDatas.Add(itemData.ID,itemData);
+            }
+
+            foreach (KeyValuePair<int, ItemData> kv in dicItemDatas)
+            {
+                Console.WriteLine(
+                    "\nID:{0},\nName:{1},\nLevel:{2},\nGoal:{3}"
+                    , kv.Key, kv.Value.name,kv.Value.level,kv.Value.goal);
+            }
+
+            /*
+            string json = File.ReadAllText("./itemdata.json");
+            Console.WriteLine(json);
+
+            ItemData[] itemdatas = JsonConvert.DeserializeObject<ItemData[]>(json);
+
+            Dictionary<int, ItemData> dicItemDatas = new Dictionary<int, ItemData>();
+
+            foreach (ItemData itemData in itemdatas)
+            {
+                Console.WriteLine("{0},{1}", itemData.ID, itemData.name);
+                dicItemDatas.Add(itemData.ID, itemData);
+            }
+
+            ItemData data = dicItemDatas[100];
+            Console.WriteLine("{0},{1}", data.ID, data.name);
+
+            /*
+           // Console.WriteLine("Hello World!");
+            Dictionary<int,string> dic = new Dictionary<int,string>();
+
+            dic.Add(100,"장검");
+            dic.Add(101,"단검");
+            dic.Add(102,"활");
+
+
+            foreach(KeyValuePair<int,string> kv in dic)
+            {
+                Console.WriteLine("key:{0},value:{1}", kv.Key, kv.Value);
+            }
+            */
+        }
+    }
+}
---
+++ Dictionary/ItemData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Dictionary
+{
+    class ItemData
+    {
+        public int ID;
+        public string name;
+        public int level;
+        public string goal;
+
+        /*
+        public ItemData(int id,string name)
+        {
+            this.ID = id;
+            this.name = name;
+        }
+        */
+    }
+}
